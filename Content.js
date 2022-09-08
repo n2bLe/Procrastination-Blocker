6c42320c445917ca7f06db9c06b75cc7e60ac858
Yasser-BJ <yasser.hollo@gmail.com>
2022-09-08 15:18:58 +0100

    initial commit

---
+++ Content.js
@@ -0,0 +1,22 @@
+
+
+switch(window.location.hostname){
+    case "www.youtube.com":
+        document.body.innerHTML = "<h1>GET BACK TO WORK </h1>"
+        break;
+    case "www.twitter.com":
+        document.body.innerHTML = "<h1>GET BACK TO WORK </h1>"
+        break;
+    case "www.facebook.com":
+        document.body.innerHTML = "<h1>GET BACK TO WORK </h1>"
+        break;
+    case "www.instagram.com":
+        document.body.innerHTML = "<h1>GET BACK TO WORK </h1>"
+        break;
+    case "www.reddit.com":
+        document.body.innerHTML = "<h1>GET BACK TO WORK </h1>"
+        break;
+    case "www.pinterest.com":
+        document.body.innerHTML = "<h1>GET BACK TO WORK </h1>"
+        break;
+}
